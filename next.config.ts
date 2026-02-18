eda51d2c6bf3b9d51983c57ce73b79fd947964e4
Aneesh Hegde <aneesh.hegde@icloud.com>
2026-02-19 00:00:48 +0530

    chore: reset

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/threads",
-        destination:
-          "https://apps.apple.com/app/apple-store/id6753618169?pt=124081099&ct=Threads&mt=8",
-        permanent: false,
-      },
-      {
-        source: "/reddit",
-        destination: "https://apps.apple.com/app/apple-store/id6753618169?pt=124081099&ct=Reddit&mt=8",
-        permanent: false
-      }
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
