d9b1c3e49b7a32b21a48af8d85a0fbf03c26aa84
Aneesh Hegde <aneesh.hegde@icloud.com>
2026-02-20 15:35:07 +0530

    feat: add campaign redirects

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,18 @@ const nextConfig: NextConfig = {
           "https://apps.apple.com/in/app/brainbits-capture-recall/id6753618169",
         permanent: false,
       },
+      {
+        source: "/threads",
+        destination:
+          "https://apps.apple.com/app/apple-store/id6753618169?pt=124081099&ct=Threads&mt=8",
+        permanent: false,
+      },
+      {
+        source: "/reddit",
+        destination:
+          "https://apps.apple.com/app/apple-store/id6753618169?pt=124081099&ct=Reddit&mt=8",
+        permanent: false,
+      },
     ];
   },
 };
